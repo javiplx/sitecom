1492e5b42310a220ce9730ce1d09f29b11295dab
Javier Palacios <javiplx@gmail.com>
2012-11-08 20:58:19 +0100

    Create /opt directory on boot filesystem

--- do_bootfs.sh
+++ do_bootfs.sh
@@ -4,17 +4,15 @@ dd if=/dev/zero of=initrd.img bs=1M count=3
 mke2fs -F -b 1024 -i 1024 initrd.img 
 tune2fs -c 0 -i 0 initrd.img 
 
-echo 'ttyS0::respawn:/sbin/getty -L ttyS0 115200 vt100' >> bootfs/etc/inittab
-
 mkdir image 
 mount -o loop -t auto initrd.img image 
 tar cC bootfs . | tar xC image 
 tar xC image -f bootfs-skel.tar
+echo 'ttyS0::respawn:/sbin/getty -L ttyS0 115200 vt100' >> image/etc/inittab
+mkdir image/opt
 umount image 
 rmdir image 
  
-sed -i -e '/^ttyS0/ d' bootfs/etc/inittab
-
 gzip -9 initrd.img 
 mv initrd.img.gz bootfs.bin
 
